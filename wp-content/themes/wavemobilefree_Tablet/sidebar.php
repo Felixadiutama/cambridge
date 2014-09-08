c6de8ff23de24994ea8fe97eea0c769b1cdd4462
Nicolas <ndalpe@gmail.com>
2014-09-08 12:23:50 +0700

    Removed useless themes

--- wp-content/themes/wavemobilefree_Tablet/sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside id="sidebar" class="clearfix">  
-
-            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('primary') ) : ?> 
-            
-        <?php endif; ?>  
-            
-</aside><!-- sidebar -->  
-<?php get_footer(); ?>
